1420c9351f0dd14c7e328b4135d959fbeb124e4a
Petr09Mitin <mpa22u514@student.bmstu.ru>
2024-11-24 12:30:25 +0300

    feat: Добавить login fixture

---
+++ hw/code/ui/fixtures.py
@@ -0,0 +1,71 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from ui.pages.auth_page import AuthPage
+from ui.pages.base_page import BasePage
+from ui.pages.main_page import MainPage
+from ui.pages.registration_page import RegistrationPage
+import os
+from dotenv import load_dotenv
+
+
+@pytest.fixture()
+def driver(config):
+    browser = config['browser']
+    url = config['url']
+    selenoid = config['selenoid']
+    vnc = config['vnc']
+    options = Options()
+    if selenoid:
+        capabilities = {
+            'browserName': 'chrome',
+            'version': '118.0',
+        }
+        if vnc:
+            capabilities['enableVNC'] = True
+        driver = webdriver.Remote(
+            'http://127.0.0.1:4444/wd/hub',
+            options=options,
+        )
+    elif browser == 'chrome':
+        driver = webdriver.Chrome()
+    elif browser == 'firefox':
+        driver = webdriver.Firefox()
+    else:
+        raise RuntimeError(f'Unsupported browser: "{browser}"')
+    driver.get(url)
+    driver.maximize_window()
+    yield driver
+    driver.quit()
+
+
+@pytest.fixture
+def base_page(driver):
+    return BasePage(driver=driver)
+
+
+@pytest.fixture
+def main_page(driver):
+    return MainPage(driver=driver)
+
+
+@pytest.fixture(scope='session')
+def credentials_without_cabinet():
+    load_dotenv()
+    return os.getenv('LOGIN_WITHOUT_CABINET'), os.getenv('PASSWORD_WITHOUT_CABINET')
+
+
+@pytest.fixture(scope='session')
+def credentials_with_cabinet():
+    load_dotenv(dotenv_path='.env')
+    return os.getenv('LOGIN'), os.getenv('PASSWORD')
+
+@pytest.fixture
+def auth_page(driver):
+    return AuthPage(driver=driver)
+
+@pytest.fixture
+def registration_page(driver, credentials_with_cabinet, auth_page):
+    driver.get(BasePage.url)
+    auth_page.login(*credentials_with_cabinet)
+    return RegistrationPage(driver=driver)
---
+++ hw/code/ui/locators/auth_page_locators.py
@@ -0,0 +1,15 @@
+from selenium.webdriver.common.by import By
+from ui.locators.base_page_locators import BasePageLocators
+
+
+class AuthPageLocators(BasePageLocators):
+    NAV_CABINET_BUTTON = (
+        By.XPATH,
+        "//a[contains(@class, 'ButtonCabinet_primary__')]"
+    )
+    MAIL_RU_AUTH_BUTTON = (By.XPATH, "//button[@data-test-id='oAuthService_mail_ru']")
+    MAIL_RU_LOGIN = (By.NAME, 'username')
+    MAIL_RUN_OTHER_WAY_BUTTON = (By.XPATH, "//*[@data-test-id='auth-screen-vkid-change-restore-type-btn']")
+    MAIL_RU_PASSWORD = (By.NAME, "password")
+    MAIL_RU_NEXT_BUTTON = (By.XPATH, "//*[@data-test-id='next-button']")
+    MAIL_RU_SUBMIT_BUTTON = (By.XPATH, "//*[@data-test-id='submit-button']")
