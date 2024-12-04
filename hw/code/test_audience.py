a10011975fa530f977e70f2c2d71f4ac47a2e0ee
Petr09Mitin <mpa22u514@student.bmstu.ru>
2024-12-04 13:57:03 +0300

    docs: update .env.example

--- hw/code/test_audience.py
+++ hw/code/test_audience.py
@@ -2,10 +2,10 @@ import pytest
 from base_case import BaseCase
 
 class TestAudience(BaseCase):
-    _valid_name="audience"
-    _valid_name2="test_name"
-    _valid_list_name="user_list"
-    _valid_list_name2="список"
+    _valid_name="audience_one"
+    _valid_name2="test_name_two"
+    _valid_list_name="user_list_one"
+    _valid_list_name2="список_пользователей_два"
     
     def test_create_audience_from_user_list(self, audience_page):
         audience_page.create_audience_from_user_list(name=self._valid_name, list_name=self._valid_list_name)
@@ -15,8 +15,8 @@ class TestAudience(BaseCase):
     
     def test_search_audience(self, audience_page):
         audience_page.create_audience_from_user_list(name=self._valid_name, list_name=self._valid_list_name)
-        audience_page.create_audience_from_user_list(name=self._valid_name2, list_name=self._valid_list_name2)
         audience_page.assert_audience_visible(name=self._valid_name)
+        audience_page.create_audience_from_user_list(name=self._valid_name2, list_name=self._valid_list_name2)
         audience_page.assert_audience_visible(name=self._valid_name2)
         
         audience_page.search_for_audience(name=self._valid_name)
