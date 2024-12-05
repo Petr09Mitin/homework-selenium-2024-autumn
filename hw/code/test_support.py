19900131349df8d2e8e5a56dcae9dbbc79aef4b7
lexa <lexagorbunov14@gmail.com>
2024-12-05 18:04:02 +0300

    HW4: delete comments from support

--- hw/code/test_support.py
+++ hw/code/test_support.py
@@ -19,27 +19,3 @@ class TestSupport(BaseCase):
         support_page.assert_references_list_visible()
         support_page.click_suggest_idea_reference()
         support_page.assert_reference_to_window(self._suggest_idea_url)
-        
-    # These tests are commented because there is unpredictable site behavior after clicking "Ask a question" button
-    # def test_ask_a_question_modal(self, support_page):
-    #     support_page.click_open_ask_a_question()
-    #     support_page.assert_ask_a_question_window_visible()
-    #     support_page.click_close_ask_a_question()
-    #     support_page.assert_ask_a_question_window_invisible()
-        
-    # def test_reference_to_help_from_modal(self, support_page):
-    #     support_page.click_open_ask_a_question()
-    #     support_page.click_reference_to_help_modal()
-    #     support_page.assert_reference_to_window(self._help_url)
-        
-    # def test_success_question(self, support_page):
-    #     support_page.click_open_ask_a_question()
-    #     support_page.click_choose_other_theme()
-    #     support_page.fill_message()
-    #     support_page.fill_name()
-    #     support_page.fill_email()
-    #     support_page.add_image()
-    #     support_page.click_submit_question()
-    #     support_page.assert_successful_question()
-    #     support_page.click_close_successful_modal()
-    #     support_page.assert_closed_modal()
